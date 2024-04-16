705308125185acd565d56ba464d049ae8b154ad2
bhuvaneshwa <bhuvaneshwaran368@gmail.com>
2024-04-16 22:06:57 +0530

    App.jsx commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Footer";
-import TopNavbar from "./pages/TopNavbar";
-import SecondNavbar from "./pages/SecondNavbar";
+
 
 // Lazy import your pages
 const LazyNavbar = React.lazy(() => import("./pages/Navbar"));
 
 const Topnavbar = React.lazy(() =>import ("./pages/TopNavbar"))
 const Secondnavbar = React.lazy(() =>import ("./pages/SecondNavbar"))
+
+
 // Loading component for Suspense fallback
 const LoadingAnimation = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="loader"></div>
+      <div className="loader">sdsdsd</div>
     </div>
   );
 };
